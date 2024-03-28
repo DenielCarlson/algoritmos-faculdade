71d03ee96c0f7bc06482ff99197add04ecba74d5
Deniel Carlson <denielcarlson_18@outlook.com>
2024-03-27 21:37:51 -0300

    Ex005 - Ler o salario base, acrescentar 5% e tirar 7% tendo como a base de calculo o salario base mais a gratificacao de 5%

---
+++ src/algoritmos/Ex005.java
@@ -0,0 +1,25 @@
+package algoritmos;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex005 {
+
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		double salarioBase, novoSalario;
+		
+		System.out.print("Salário base: ");
+		salarioBase = Double.parseDouble(input.nextLine());
+		
+		novoSalario = (salarioBase * 1.05)* 0.93;
+		
+		System.out.println("Salário a receber: " + novoSalario);
+		
+		input.close();
+	}
+}
