225b286f21a380a34a5b7add2ae0d3d2dcaf3e16
Deniel Carlson <denielcarlson_18@outlook.com>
2024-03-27 21:16:41 -0300

    Ex002 - Media aritmetica de tres notas

---
+++ src/algoritmos/Ex002.java
@@ -0,0 +1,31 @@
+package algoritmos;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex002 {
+
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		double media;
+		double sum = 0d;
+		
+		for(int i = 0; i < 3; i++) {
+			
+			System.out.print("Nota " + (i + 1) + ": ");
+			double nota = Double.parseDouble(input.nextLine());
+			
+			sum += nota;
+		}
+		
+		media = sum/3;
+		
+		System.out.println("A média aritmética das notas é igual a " + media);
+		
+		input.close();
+	}
+}
