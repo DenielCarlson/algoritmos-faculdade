1c8ec8830c669e7b6cd3a9b30bd3b8e6a854a89e
Deniel Carlson <denielcarlson_18@outlook.com>
2024-04-04 11:20:39 -0300

    Numero primo

---
+++ src/algoritmos/NumeroPrimo.java
@@ -0,0 +1,39 @@
+package algoritmos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NumeroPrimo {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner input = new Scanner(System.in);
+		
+		List<Integer> divisores = new ArrayList<Integer>();
+		int valor;
+		
+		System.out.print("Digite um valor: ");
+		valor = Integer.parseInt(input.nextLine());
+		
+		for(int i = 1; i <= valor; i++) {
+			
+			if(valor % i == 0) {
+				divisores.add(i);
+				
+			}
+		}
+		
+		if(divisores.size() == 2) {
+			
+			System.out.print(valor + " e primo");
+		}else {
+			System.out.print(valor + " nao e primo");
+		}
+		
+		input.close();
+	}
+}
